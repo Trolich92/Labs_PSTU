c78d54de04c5921a32148adb25e65d4d3987b262
Trolich92 <63595086+Trolich92@users.noreply.github.com>
2020-04-16 14:58:13 +0300

    Add files via upload

---
+++ Лабораторная работа №15/Лабораторная работа №15.cpp
@@ -0,0 +1,60 @@
+﻿//Артём Чупров, лаб.15, Сортировка стека через stl-контейнеры
+#include <iostream>
+#include <ctime>
+#include <stack>
+using namespace std;
+bool check(stack<int> input)
+{
+	int p = input.top();
+	input.pop();
+	while (!input.empty())
+	{
+		if (p != input.top())
+			return true;
+		input.pop();
+	}
+}
+stack<int> sortStack(stack<int> input)
+{
+	stack<int> tmpStack;
+	int p = input.top();
+
+	while (!input.empty())
+	{
+		int tmp = input.top();
+		input.pop();
+		while (!tmpStack.empty() && tmpStack.top() < tmp)
+		{
+			input.push(tmpStack.top());
+			tmpStack.pop();
+		}
+		tmpStack.push(tmp);
+	}
+	return tmpStack;
+}
+int main()
+{
+	srand((unsigned)NULL);
+	setlocale(LC_ALL, "ru");
+	stack<int> input;
+	int N, p;
+	cout << "Введите число элементов: ";
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> p;
+		input.push(p);
+	}
+	stack<int> tmpStack = sortStack(input);
+	if (check(input))
+	{
+		cout << "\nОтсортированный стэк:\n";
+		while (!tmpStack.empty())
+		{
+			cout << tmpStack.top() << " ";
+			tmpStack.pop();
+		}
+	}
+	else
+		cout << "\nМассив состоит из одинаковых элементов, равных " << input.top();
+}
